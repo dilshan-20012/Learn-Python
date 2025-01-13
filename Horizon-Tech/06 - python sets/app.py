c547a0a0de2c2e63b1aa29026be1059df8e07038
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-13 21:35:05 +0530

    set - create set from a string

--- Horizon-Tech/06 - python sets/app.py
+++ Horizon-Tech/06 - python sets/app.py
@@ -26,3 +26,7 @@ print(my_set5)
 # We can make set from a tuple
 my_set6 = set((1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 print(my_set6)
+
+# We can make set from a string
+my_set7 = set("Hello")
+print(my_set7)
