07368638839a4742cef5ca5b0fbd6d35bffdd84a
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-13 21:34:45 +0530

    set - create set from a tuple

--- Horizon-Tech/06 - python sets/app.py
+++ Horizon-Tech/06 - python sets/app.py
@@ -22,3 +22,7 @@ print(my_set3)
 # We can make set from a list
 my_set5 = set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 print(my_set5)
+
+# We can make set from a tuple
+my_set6 = set((1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+print(my_set6)
