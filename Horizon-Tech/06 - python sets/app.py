f1b8147b26e372d1bb6d2fe6fe12e1f30edf9c0c
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-13 21:26:21 +0530

    define set in python

---
+++ Horizon-Tech/06 - python sets/app.py
@@ -0,0 +1,7 @@
+# Python Sets
+# A set is a collection which is unordered and unindexed. In Python sets are written with curly brackets.
+# Sets are unordered, so you cannot be sure in which order the items will appear.
+# Set items are unordered, and do not allow duplicate values.
+
+my_set1 = {11, 33, 66, 55, 22}
+print(my_set1)
