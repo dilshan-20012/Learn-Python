98c81bc39d634a43dacbe3a3f103cb39a30e7414
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-14 17:39:22 +0530

    create new directory

--- Horizon-Tech/08 - python directory and file management/app.py
+++ Horizon-Tech/08 - python directory and file management/app.py
@@ -3,3 +3,8 @@ import os
 # looking current directory
 print(os.getcwd()) #C:\Learn\Learn-Python\Horizon-Tech\08 - python directory and file management
 print(os.getcwdb()) #b'C:\\Learn\\Learn-Python\\Horjson-Tech\\08 - python directory and file management'
+
+
+# Create the new directory
+os.mkdir('new_dir_01')
+
