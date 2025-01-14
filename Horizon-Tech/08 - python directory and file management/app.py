aa135f6ad699c2c6e8c35304b4965b5c188ab17c
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-14 17:50:38 +0530

    un comment commented codes

--- Horizon-Tech/08 - python directory and file management/app.py
+++ Horizon-Tech/08 - python directory and file management/app.py
@@ -6,19 +6,19 @@ print(os.getcwdb()) #b'C:\\Learn\\Learn-Python\\Horjson-Tech\\08 - python direct
 
 
 # Create the new directory
-# os.mkdir('new_dir_01')
+os.mkdir('new_dir_01')
 
 # Rename the directory
 # os.rename('new_dir_0', 'new_dir_02') # FileNotFoundError: [WinError 2] The system cannot find the file specified: 'new_dir_0' -> 'new_dir_02'
-# os.rename('new_dir_01', 'my_new_dir_02')
+os.rename('new_dir_01', 'my_new_dir_02')
 
 # List the file and directory in the current directory
 print(os.listdir()) # ['app.py', 'my_new_dir_02']
 
 # Remove the directory and file
 # os.rmdir('my_new_dir_00') # FileNotFoundError: [WinError 2] The system cannot find the file specified: 'my_new_dir_00'
-# os.rmdir('my_new_dir_02')
-# os.rmdir('new_dir_01')
+os.rmdir('my_new_dir_02')
+os.rmdir('new_dir_01')
 
 # change the directory
 print(os.getcwd())
