7460de1e4f2b6bc31d40e3713cbcb618bc059775
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-14 17:41:24 +0530

    rename the existing file

--- Horizon-Tech/08 - python directory and file management/app.py
+++ Horizon-Tech/08 - python directory and file management/app.py
@@ -8,3 +8,6 @@ print(os.getcwdb()) #b'C:\\Learn\\Learn-Python\\Horjson-Tech\\08 - python direct
 # Create the new directory
 os.mkdir('new_dir_01')
 
+# Rename the directory
+# os.rename('new_dir_0', 'new_dir_02') # FileNotFoundError: [WinError 2] The system cannot find the file specified: 'new_dir_0' -> 'new_dir_02'
+os.rename('new_dir_01', 'my_new_dir_02')
