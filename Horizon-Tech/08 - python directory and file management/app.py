696d612bfdc2139722c03df625231cdd6e86c44e
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-14 17:39:02 +0530

    current directory in string and byte format

---
+++ Horizon-Tech/08 - python directory and file management/app.py
@@ -0,0 +1,5 @@
+import os
+
+# looking current directory
+print(os.getcwd()) #C:\Learn\Learn-Python\Horizon-Tech\08 - python directory and file management
+print(os.getcwdb()) #b'C:\\Learn\\Learn-Python\\Horjson-Tech\\08 - python directory and file management'
