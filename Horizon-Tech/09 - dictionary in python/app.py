e48f9f9a8a80b5902159d6785470d396f1aea3fb
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-15 21:06:27 +0530

    dictionary - clear all items

--- Horizon-Tech/09 - dictionary in python/app.py
+++ Horizon-Tech/09 - dictionary in python/app.py
@@ -43,3 +43,7 @@ dic_01.pop('age') # remove particular item, if key is present. Otherwise, it wil
 dic_01 = {'name': 'John', 'age': 25, 'city': 'New York'}
 del dic_01['age'] # remove particular item
 del dic_01 # delete entire dictionary
+
+# remove all items
+dic_01 = {'name': 'John', 'age': 25, 'city': 'New York'}
+dic_01.clear()
