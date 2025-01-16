14d801ecffa717b99d89287569832b8c7678b173
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-16 20:49:23 +0530

    string - accessing the character fo the string

--- Horizon-Tech/10 - string/app.py
+++ Horizon-Tech/10 - string/app.py
@@ -7,3 +7,13 @@ print("""Hello World""")
 # triple quotes are used to define multi-line strings
 print('''Hello
  World''')
+
+# accessing the character of the string
+print("Hello"[0])
+print("Hello"[1])
+# print("Hello"[10]) # IndexError: string index out of range
+print("Hello"[-1])
+print("Hello"[0:10])
+print("Hello"[10:10])
+print("Hello"[:-2])
+print("Hello"[-2:-1])
