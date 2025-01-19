14edb551b7c18bf5a70a978e13a335be44a62917
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-19 21:10:06 +0530

    numbers - define numbers in the variable and check the instance

---
+++ Horizon-Tech/12 - python numbers/app.py
@@ -0,0 +1,8 @@
+# Numbers in python
+value1 = 100
+print(type(value1))
+print(value1)
+print(isinstance(value1, int)) # True
+print(isinstance(value1, float)) # False
+print(isinstance(value1, str)) # False
+print(isinstance(value1, complex)) # False
