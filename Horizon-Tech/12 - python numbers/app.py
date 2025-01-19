cc3126818a793b83c16ea60aec4bc6e267789fcb
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-01-19 21:12:07 +0530

    numbers - float type in numbers

--- Horizon-Tech/12 - python numbers/app.py
+++ Horizon-Tech/12 - python numbers/app.py
@@ -1,8 +1,16 @@
 # Numbers in python
 value1 = 100
+# Integer
 print(type(value1))
 print(value1)
 print(isinstance(value1, int)) # True
 print(isinstance(value1, float)) # False
 print(isinstance(value1, str)) # False
 print(isinstance(value1, complex)) # False
+
+# Float
+value2 = 100.0
+print(type(value2))
+print(value2)
+print(isinstance(value2, int)) # False
+print(isinstance(value2, float)) # True
